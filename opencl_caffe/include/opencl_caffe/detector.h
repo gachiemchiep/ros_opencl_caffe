cf18411a35c1e4c0f74c08bf1b4efc2f9f839cc1
gachiemchiep <gachiemchiep@gmail.com>
2021-03-01 19:49:19 +0900

    Add structure to store configuration

--- opencl_caffe/include/opencl_caffe/detector.h
+++ opencl_caffe/include/opencl_caffe/detector.h
@@ -23,6 +23,25 @@
 
 namespace opencl_caffe
 {
+/**
+ * A structure to store configuration for Detector
+*/
+struct DetectorConfig
+{
+  std::string model;      // Neural network weights file path
+  std::string config;     // Network configuration file path
+  std::string classes;    // File path of labels of network output classes
+  cv::Scalar mean = cv::Scalar(0, 0, 0);        // "Preprocess input image by subtracting mean values. 
+  // default value for yolo : https://github.com/arunponnusamy/object-detection-opencv/issues/5
+  float scale = 0.00392;  // "Preprocess input image by multiplying on a scale factor."
+  float thr = 0.5;        // Confidence threshold.
+  float nms = 0.4;        // Non-maximum suppression threshold.
+  // OpenCV use BGR
+  bool swapRB = True;     // Indicate that model works with RGB input images instead BGR ones.
+  cv::Size inpWidth = cv::Size(416, 416);      // Preprocess input image that has size (w, h)
+  int backend = dnn::DNN_BACKEND_CUDA;  // Use cuda as default backend
+  int target = dnn::DNN_TARGET_CUDA;    // use cuda 32 as default target
+};
 /** @class Detector
  * @brief Base class for detecting.
  * This class define a common interface of nueral network inference.
@@ -42,7 +61,7 @@ public:
    * @param[in]   labels    File path of labels of network output classes
    * @return     Status of load resources, true for success or false for failed
    */
-  virtual int loadResources(const std::string& net_cfg, const std::string& weights, const std::string& labels) = 0;
+  virtual int loadResources(const opencl_caffe::DetectorConfig& config) = 0;
   /**
    * Public interface of running inference to infer all objects in image.
    *
